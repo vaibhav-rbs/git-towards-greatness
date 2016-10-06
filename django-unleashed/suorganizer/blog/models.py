42e13743bee4059b1f30044ff560d60b691cdc7a
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-10-06 11:14:40 -0700

    3.6.1 Basic Database Interaction via Models

--- django-unleashed/suorganizer/blog/models.py
+++ django-unleashed/suorganizer/blog/models.py
@@ -2,9 +2,23 @@ from django.db import models
 from organizer.models import Startup, Tag
 
 class Post(models.Model):
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
     title = models.CharField(max_length=63)
-    slug = models.SlugField()
+    slug = models.SlugField(
+        max_length=63,
+        help_text='A lable for URL config.',
+        unique_for_month='pub_date'
+    )
     text = models.TextField()
-    pub_date = models.DateField()
+    pub_date = models.DateField('date published', auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
+
+    def __str__(self):
+        return "{} on {}".format(
+            self.title, self.pub_date.strftime('%Y-%m-%d')
+        )
+
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
--- django-unleashed/suorganizer/organizer/models.py
+++ django-unleashed/suorganizer/organizer/models.py
@@ -1,20 +1,50 @@
 from django.db import models
 
 class Tag(models.Model):
-    name = models.CharField(max_length=31)
-    slug = models.SlugField()
+    name = models.CharField(
+        max_length=31, unique=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text='A lable for URL config'
+    )
+    class Meta:
+        ordering  = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Startup(models.Model):
     tags = models.ManyToManyField(Tag)
-    name = models.CharField(max_length=31)
-    slug = models.SlugField()
+    name = models.CharField(
+        max_length=31, db_index=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text='A lable for URL config.'
+    )
     description = models.TextField()
-    founded_date = models.DateField()
+    founded_date = models.DateField('date founded')
     contact = models.EmailField()
     website = models.URLField()
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
     
 class NewsLink(models.Model):
-    startup = models.ForiegnKey(Startup)
+    startup = models.ForeignKey(Startup)
     title = models.CharField(max_length=63)
-    pub_date = models.DateField()
-    link = models.URLfield()
+    pub_date = models.DateField('date published')
+    link = models.URLField(max_length=255)
+    
+    class Meta:
+        verbose_name = 'news article'
+        ordering = ['-pub_date']
+        get_latest_by = 'pub_date'
+
+    def __str__(self):
+        return "{}:{}".format(
+            self.startup, self.title)
