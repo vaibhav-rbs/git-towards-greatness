9ad633d0697cbef250b414fa30b5fc57b26962c9
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-10-20 08:27:34 -0700

    5.7.2 Using include to Create a Hierarchy of URL Configurations

--- django-unleashed/suorganizer/organizer/views.py
+++ django-unleashed/suorganizer/organizer/views.py
@@ -1,12 +1,17 @@
-from django.http.response import HttpResponse
 from .models import Tag
-from django.template import Context, loader
+from django.shortcuts import (get_object_or_404, render)
 
 
 def homepage(request):
-	tag_list = Tag.objects.all()
-	template = loader.get_template(
-		'organizer/tag_list.html')
-	context = Context({'tag_list':tag_list})
-	output = template.render(context)
-	return HttpResponse(output)
+	return render(
+		request,
+		'organizer/tag_list.html',
+		{'tag_list':Tag.objects.all()})
+
+
+def tag_detail(request, slug):
+	tag = get_object_or_404(
+		Tag, slug__iexact=slug)
+	return render(
+		request, 'organizer/tag_detail.html',
+		{'tag':tag})
--- django-unleashed/suorganizer/suorganizer/urls.py
+++ django-unleashed/suorganizer/suorganizer/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage
+from organizer.views import homepage, tag_detail
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', homepage),
+    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
 ]
