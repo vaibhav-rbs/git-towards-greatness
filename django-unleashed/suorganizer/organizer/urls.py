164214f6482bd016029f2830b56859266311bf5b
vaibhav-rbs <vaibhav@ixsystems.com>
2016-10-26 15:14:35 -0700

    removed conflicts

--- django-unleashed/suorganizer/organizer/urls.py
+++ django-unleashed/suorganizer/organizer/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 
-<<<<<<< HEAD
 from .views import (
     startup_list, tag_detail, tag_list, startup_detail
 )
@@ -19,13 +18,3 @@ urlpatterns = [
     tag_detail,
     name='organizer_tag_detail'),
 ]
-=======
-from .views import homepage, tag_detail
-
-urlpatterns = [
-    url(r'^$', homepage),
-    url(r'^tag/(?P<slug>[\w\-]+)/$',
-    tag_detail,
-    name='organizer_tag_detail')
-]
->>>>>>> 00d1a2a71cd4a0887ab9b92b796cc9e9bf88ed16
